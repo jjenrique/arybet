a138b2c7349762302ab44a29d509693af51c9ff3
jjenrique <jjauda@uc.cl>
2023-08-02 23:13:53 -0400

    La app tiene login, clientes, cotizaciones y agregar_clientes. No tiene la mejor interfaz. Ya podriamos hacer deploy para luego hacer agregar_cotizaciones. Falta mejorar la tabla cotizaciones para que salga anidada

--- src/app.py
+++ src/app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager, login_user, logout_user, login_required
-
+from flask_paginate import Pagination, get_page_args
 from config import config
 
 # Models:
-from models.ModelUser import ModelUser
+from models.ModelUser import ModelUser, Cliente, Cotizacion
 
 # Entities:
 from models.entities.User import User
@@ -38,6 +38,7 @@ def login():
         if logged_user != None:
             if logged_user.password:
                 login_user(logged_user)
+                
                 return redirect(url_for('home'))
             else:
                 flash("Invalid password...")
@@ -65,6 +66,50 @@ def home():
 def protected():
     return "<h1>Esta es una vista protegida, solo para usuarios autenticados.</h1>"
 
+@app.route('/clientes', methods=['GET'])
+def clientes():
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    
+    clientes = Cliente.obtener_todos(db, offset=offset, limit=per_page)
+    
+    total_clientes = 100
+    
+    pagination = Pagination(page=page, per_page=per_page, total=total_clientes, css_framework='bootstrap5')
+    
+    return render_template('clientes.html', clientes=clientes,page=page,pager_page=per_page,pagination=pagination)
+
+@app.route('/cotizaciones', methods=['GET'])
+def cotizaciones():
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    
+    cotizaciones = Cotizacion.obtener_todos(db, offset=offset, limit=per_page)
+    
+    total_cotizaciones = 100
+    
+    pagination = Pagination(page=page, per_page=per_page, total=total_cotizaciones, css_framework='bootstrap5')
+    
+    return render_template('cotizaciones.html', cotizaciones=cotizaciones,page=page,pager_page=per_page,pagination=pagination)
+
+@app.route('/ingresar_cliente', methods=['GET', 'POST'])
+def mostrar_formulario_cliente():
+    if request.method == 'POST':
+        # Obtener los datos ingresados en el formulario
+        razon_social = request.form['razon_social']
+        rut = request.form['rut']
+        telefono = request.form['telefono']
+        email = request.form['email']
+        persona_de_contacto = request.form['persona_de_contacto']
+
+        # Crear un nuevo objeto Cliente con los datos ingresados
+        nuevo_cliente = Cliente(razon_social, rut, telefono, email, persona_de_contacto)
+
+        # Insertar el nuevo cliente en la base de datos
+        nuevo_cliente.insertar(db)
+
+        # Redirigir a la página de lista de clientes después de agregar el nuevo cliente
+        return redirect(url_for('clientes'))
+
+    return render_template('ingresar_cliente.html')
 
 def status_401(error):
     return redirect(url_for('login'))
--- src/models/ModelUser.py
+++ src/models/ModelUser.py
@@ -12,7 +12,8 @@ class ModelUser():
             cursor.execute(sql)
             row = cursor.fetchone()
             if row != None:
-                user = User(row[0], row[1], User.check_password(row[2], user.password), row[3])
+                user = User(row[0], row[1], User.check_password(
+                    row[2], user.password), row[3])
                 return user
             else:
                 return None
@@ -23,7 +24,8 @@ class ModelUser():
     def get_by_id(self, db, id):
         try:
             cursor = db.connection.cursor()
-            sql = "SELECT id, username, fullname FROM user WHERE id = {}".format(id)
+            sql = "SELECT id, username, fullname FROM user WHERE id = {}".format(
+                id)
             cursor.execute(sql)
             row = cursor.fetchone()
             if row != None:
@@ -32,3 +34,57 @@ class ModelUser():
                 return None
         except Exception as ex:
             raise Exception(ex)
+
+
+class Cliente:
+    def __init__(self, razon_social, rut, telefono, email, persona_de_contacto):
+        self.razon_social = razon_social
+        self.rut = rut
+        self.telefono = telefono
+        self.email = email
+        self.persona_de_contacto = persona_de_contacto
+
+    @classmethod
+    def obtener_todos(self, mysql, offset, limit):
+        connection = mysql.connection
+        cursor = connection.cursor()
+        cursor.execute("SELECT razon_social, rut, telefono, email, persona_de_contacto FROM clientes ORDER BY id DESC LIMIT %s OFFSET %s", (limit, offset))
+        resultados = cursor.fetchall()
+        cursor.close()
+
+        clientes = []
+        for resultado in resultados:
+            cliente = self(resultado[0], resultado[1], resultado[2], resultado[3], resultado[4])
+            clientes.append(cliente)
+
+        return clientes
+    
+    def insertar(self, mysql):
+        connection = mysql.connection
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO clientes (razon_social, rut, telefono, email, persona_de_contacto) VALUES (%s, %s, %s, %s, %s)",
+                       (self.razon_social, self.rut, self.telefono, self.email, self.persona_de_contacto))
+        connection.commit()
+        cursor.close()
+
+class Cotizacion:
+    def __init__(self, id, fecha, fecha_entrega, cliente_id):
+        self.id = id
+        self.fecha = fecha
+        self.fecha_entrega = fecha_entrega
+        self.cliente_id = cliente_id
+
+    @classmethod
+    def obtener_todos(cls, mysql, offset, limit):
+        connection = mysql.connection
+        cursor = connection.cursor()
+        cursor.execute("SELECT id, fecha, fecha_entrega, cliente_id FROM cotizaciones ORDER BY id ASC LIMIT %s OFFSET %s", (limit, offset))
+        resultados = cursor.fetchall()
+        cursor.close()
+
+        cotizaciones = []
+        for resultado in resultados:
+            cotizacion = cls(resultado[0], resultado[1], resultado[2], resultado[3])
+            cotizaciones.append(cotizacion)
+
+        return cotizaciones
